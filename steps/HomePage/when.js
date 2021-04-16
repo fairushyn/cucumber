49f4917611082f0aac8e442594816faa01cf2503
Roman Fairushyn <fyrushin_roman@yahoo.com>
2021-04-16 09:37:21 +0300

    - added support files; updated step definitions to use support files

--- steps/HomePage/when.js
+++ steps/HomePage/when.js
@@ -1,9 +1,6 @@
 import { When } from "cucumber";
+import search from "../../support/actions/search";
 
 When(/^The user enters "(.*)" into the search bar$/, keyword => {
-    $(".gLFyf.gsfi").waitForDisplayed(5000);
-    $(".gLFyf.gsfi").click();
-    $(".gLFyf.gsfi").setValue(keyword);
-    $(".aajZCb .gNO89b").waitForDisplayed(5000);
-    $(".aajZCb .gNO89b").click();
+    search(keyword, $(".gLFyf.gsfi"), $(".aajZCb .gNO89b"));
 });
