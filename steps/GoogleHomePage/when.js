836ae816f7ab6b6e5883a8922eed6b313f024bf1
Roman Fairushyn <fyrushin_roman@yahoo.com>
2021-04-16 13:45:23 +0300

    add Feature: Menu Navigation

--- steps/GoogleHomePage/when.js
+++ steps/GoogleHomePage/when.js
@@ -1,6 +1,12 @@
 import { When } from "cucumber";
 import search from "../../support/actions/googleSearch";
+import clickMenuOption from "../../support/actions/clickMenuOption";
 
 When(/^The user enters "(.*)" into the search bar$/, keyword => {
     search(keyword);
 });
+
+When(/^the user clicks on the "(.*)" option in the menu$/, option => {
+    clickMenuOption(option);
+});
+
