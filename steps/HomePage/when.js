f2cfc64c182c24c23c24ad354e25035dce39d5bc
Roman Fairushyn <fyrushin_roman@yahoo.com>
2021-04-16 09:48:36 +0300

    added page objects and updated code to use them

--- steps/HomePage/when.js
+++ steps/HomePage/when.js
@@ -2,5 +2,5 @@ import { When } from "cucumber";
 import search from "../../support/actions/search";
 
 When(/^The user enters "(.*)" into the search bar$/, keyword => {
-    search(keyword, $(".gLFyf.gsfi"), $(".aajZCb .gNO89b"));
+    search(keyword);
 });
--- steps/ResultsPage/then.js
+++ steps/ResultsPage/then.js
@@ -1,7 +1,6 @@
 import { Then } from "cucumber";
-import assert from "assert";
 import verifyLinksContain from "../../support/assertions/verifyLinksContain";
 
 Then(/^links related to "(.*)" are shown on the results page$/, keyword => {
-    verifyLinksContain($$(".LC20lb"), keyword);
+    verifyLinksContain(keyword);
 });
